23f6ecea553ef69e13a5b148d4bdfd6461dec255
wangjunying <wangjunying@seeking.ai>
2021-12-08 15:36:22 +0800

    updated author

--- .umirc.ts
+++ .umirc.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'w-react-ts',
-  favicon:
-    '/img/logo.png',
+  favicon: '/img/logo.png',
   logo: '/img/logo.png',
   outputPath: 'docs-dist',
   // more config: https://d.umijs.org/config
