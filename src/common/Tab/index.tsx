619501e0387c732b57836bf0e45aaf2efaadac85
wang389846 <wang389846@seeking.ai>
2023-09-19 18:38:47 +0800

    updated V1.0.50。tab可以横向滚动

--- src/common/Tab/index.tsx
+++ src/common/Tab/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ReactNode } from 'react';
+import React, { Component, ReactNode, createRef } from 'react';
 
 import WTabPane from './WTabPane';
 import './index.less';
@@ -22,8 +22,10 @@ class WTab extends Component<Props> {
   state = {
     curTag: '',
   };
+  tabRef: React.RefObject<HTMLDivElement> = createRef();
 
   componentDidMount() {
+    this.tabRef?.current?.addEventListener('wheel', this.wheelTab, { passive: false });
     this.init();
   }
 
@@ -31,6 +33,10 @@ class WTab extends Component<Props> {
     if (this.props.curTag !== prevProps.curTag) {
       this.init();
     }
+  }
+
+  componentWillUnmount(): void {
+    this.tabRef?.current?.removeEventListener('wheel', this.wheelTab);
   }
 
   init = () => {
@@ -50,6 +56,34 @@ class WTab extends Component<Props> {
     this.props.onClick && this.props.onClick(tag, index, item);
   };
 
+  // 横向滚动
+  wheelTab = (event: any) => {
+    event?.stopPropagation();
+    event.preventDefault();
+
+    const scrollValue = event.deltaY > 0 ? 100 : -100;
+    if (this.tabRef?.current) {
+      this.tabRef.current.scrollLeft += scrollValue;
+    }
+
+    // if(!this.tabRef?.current) return;
+
+    // // 判断是否已经滚动到最左边
+    // if (this.tabRef.current?.scrollLeft === 0) {
+    //   // console.log('已经滚动到最左边');
+    // }
+
+    // // 判断是否已经滚动到最右边
+    // if (this.tabRef.current?.scrollLeft === (this.tabRef.current?.scrollWidth - this.tabRef.current?.clientWidth)) {
+    //   // console.log('已经滚动到最右边');
+    // }
+
+    // // 判断是否出现垂直滚动条
+    // if (this.tabRef.current?.scrollWidth > this.tabRef.current?.clientWidth) {
+    //   // console.log('内容超出容器高度，可能会出现垂直滚动条');
+    // }
+  };
+
   render() {
     let { children, className, extraNode } = this.props,
       { curTag } = this.state,
@@ -58,7 +92,7 @@ class WTab extends Component<Props> {
       <div className="tab-box">
         <div className={`t-top ${className}`}>
           {extraNode?.['left' as keyof typeof extraNode]}
-          <div className="t-top-center">
+          <div className="t-top-center" ref={this.tabRef}>
             {React.Children.map(children, (item: any, i) => {
               if (!item) return;
               let { label, tag } = item?.props;
